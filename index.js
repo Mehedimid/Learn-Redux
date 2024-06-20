ec3ec1c925e584c96fd938ece1f1535b91eddd6d
mehedimid <mehedihasan25276@gmail.com>
2024-06-21 02:07:09 +0600

    install redux and store, dispatch, action, reducer, dispatch

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello redux")
+import {createStore} from "redux" ;
+
+// creating store 
+const store = createStore(reducer)
+
+// creating a reducer function 
+function reducer(state={amount:1}, action){
+    if(action.type=="increase"){
+        return {amount : state.amount+1}
+    }
+    return state
+}
+
+// global state 
+console.log(store.getState())
+
+store.dispatch({type:"increase"})
+
+console.log(store.getState())
