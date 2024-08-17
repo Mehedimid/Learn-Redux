8070971ea4ab7e54554d5ac14a2da4233889f1e2
mehedimid <mehedihasan25276@gmail.com>
2024-08-17 23:00:10 +0600

    old pc last commit

--- index.js
+++ index.js
@@ -1,19 +1,57 @@
-import {createStore} from "redux" ;
+import axios from "axios";
+import logger from "redux-logger";
+import  { thunk }  from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+
+ 
+const history = [];
+
+const INIT = "init";
+const INCREASE = 'increase';
+const DECREASE = 'decrease';
+const INCREASE_BY_AMOUNT = 'increaseByAmount';
 
 // creating store 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(logger.default, thunk));
 
 // creating a reducer function 
-function reducer(state={amount:1}, action){
-    if(action.type=="increase"){
-        return {amount : state.amount+1}
+function reducer(state = { amount: 1 }, action) {
+    switch (action.type) {
+        case INIT:
+            return { amount: action.payload };
+        case INCREASE:
+            return { amount: state.amount + 1 };
+        case DECREASE:
+            return { amount: state.amount - 1 };
+        case INCREASE_BY_AMOUNT:
+            return { amount: state.amount + action.payload };
+        default:
+            return state;
     }
-    return state
 }
 
-// global state 
-console.log(store.getState())
+// ==== action creators ====
+async function initUser(dispatch, getState) {
+    const { data } = await axios.get("http://localhost:3000/accounts"); 
+    dispatch({ type: INIT, payload: data.amount });
+}
 
-store.dispatch({type:"increase"})
+function increaseAct() {
+    return { type: INCREASE };
+}
+function decreaseAct() {
+    return { type: DECREASE };
+}
+function increaseByAmountAct(value) {
+    return { type: INCREASE_BY_AMOUNT, payload: value };
+}
 
-console.log(store.getState())
+// === dispatch an action here ===
+store.dispatch(initUser);
+
+// Uncomment to track state changes
+// store.subscribe(() => {
+//    const currentState = store.getState();
+//    history.push(currentState);
+//    console.log(history);
+// });
